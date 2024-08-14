e35d4ab46bc3c5c24807b203b88f1affb6cfb9d2
awwalfareed <mohd.fareed1122@gmail.com>
2024-08-14 16:40:53 +0530

    first-commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<div align="center">
+
+  <h2 align="center">Muhammad Fareed - Personal Portfolio</h2>
+
+
+<a href="https://awwalfareed.vercel.app/"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Osama Portfolio Desktop Demo](./public/readme-images/portfolio.png "Desktop Demo")
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+### Run Locally
+
+To run **portfolio** locally, run this command on your git bash:
+
+
+### License
+
+This project is **free to use** and does not contains any license.
---
+++ constants/constants.js
@@ -0,0 +1,111 @@
+export const NAME = 'Muhammad Fareed';
+
+export const DESIGNATION = `Full Stack Developer | MERN Stack | MEAN Stack | JavaScript | TypeScript | NextJS | NodeJS | NestJS| MongoDB | MySQL | PostgresQL`;
+
+export const DETAILS = {
+  Residence: 'India',
+  City: 'Pune',
+  // Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '90%',
+  },
+  {
+    title: 'MEAN Stack Developer',
+    level: '90%',
+  },
+  {
+    title: 'JavaScript',
+    level: '90%',
+  },
+  {
+    title: 'TypeScript',
+    level: '90%',
+  },
+  {
+    title: 'NEXTJS',
+    level: '90%',
+  },
+  {
+    title: 'ReactJS',
+    level: '95%',
+  },
+  {
+    title: 'Tailwind CSS & Bootstrap',
+    level: '90%',
+  },
+  {
+    title: 'Material UI & Angular Material',
+    level: '90%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '80%',
+  },
+  {
+    title: 'NodeJS',
+    level: '80%',
+  },
+  {
+    title: 'NestJS',
+    level: '50%',
+  },
+  {
+    title: 'MongoDB',
+    level: '80%',
+  },
+  {
+    title: 'MySQL',
+    level: '70%',
+  },
+  {
+    title: 'PostgreSQL',
+    level: '50%',
+  },
+  {
+    title: 'AWS(EC2, S3 Bucket)',
+    level: '60%',
+  },
+  {
+    title: 'Figma',
+    level: '80%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'iosamajavaid@gmail.com',
+  PHONE: '+923159591822',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/osamajavaid',
+  LINKEDIN: 'https://www.linkedin.com/in/iosamajavaid/',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
